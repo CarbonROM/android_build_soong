fb7a4297d0aeccf56397f642ab292aaaffa0fb5a
Pirama Arumuga Nainar <pirama@google.com>
2017-10-04 16:47:29 -0700

    Explicitly link the profile runtime during PGO
    Bug: http://b/65598278
    
    The profile runtime depends on libgcc for some symbols (only under some
    circumstances - armv5, ndk r14, static executables).  Since Android
    build passes -nostdlib and adds libgcc manually, the profile runtime
    gets passed to the linker later than libgcc.
    
    Instead, explicitly add the profile runtime to the linker command (and
    pass one other flag added by the clang driver to the link).
    
    Test: Build a library with profile instrumentation that otherwise fails
    instrumented build.
    
    Change-Id: I24b34cebd2c3bb6a540f8f4c465ace1be4eb90f3
    Signed-off-by: Pirama Arumuga Nainar <pirama@google.com>

--- cc/config/toolchain.go
+++ cc/config/toolchain.go
@@ -227,6 +227,10 @@ func ThreadSanitizerRuntimeLibrary(t Toolchain) string {
 	return SanitizerRuntimeLibrary(t, "tsan")
 }
 
+func ProfileRuntimeLibrary(t Toolchain) string {
+	return SanitizerRuntimeLibrary(t, "profile")
+}
+
 func ToolPath(t Toolchain) string {
 	if p := t.ToolPath(); p != "" {
 		return p
