d0b3f071b0860e393bc0c3a19109b0fd4b6b794c
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:00:40 -0700

    soong: Add function to return camera parameters library name
    Change-Id: I3113ea6813b82c3f6b250205080030ff9c2255ec

--- android/config.go
+++ android/config.go
@@ -1070,6 +1070,10 @@ func (c *deviceConfig) JavaCoverageEnabledForPath(path string) bool {
 	return coverage
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.productVariables.Carbon.Specific_camera_parameter_library)
+}
+
 // Returns true if gcov or clang coverage is enabled.
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.productVariables.GcovCoverage) ||
