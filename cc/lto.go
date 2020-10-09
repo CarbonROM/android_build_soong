c7db95771145b853a644cd0ad7e6fe004f4b8175
kazerine <kamikaze.mrinal@gmail.com>
2020-10-09 18:30:54 +0530

    soong: update lto flags
    *fix build
    
    Change-Id: I41a3737651e8b59ffc12ac35ba4218a7daa17f94

--- cc/lto.go
+++ cc/lto.go
@@ -114,13 +114,13 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 			// Set appropriate ThinLTO cache policy
 			cacheDirFormat := "-Wl,-plugin-opt,cache-dir="
 			cacheDir := android.PathForOutput(ctx, "thinlto-cache").String()
-			flags.LdFlags = append(flags.LdFlags, cacheDirFormat+cacheDir)
+			flags.Local.LdFlags = append(flags.Local.LdFlags, cacheDirFormat+cacheDir)
 
 			// Limit the size of the ThinLTO cache to the lesser of 10% of available
 			// disk space and 10GB.
 			cachePolicyFormat := "-Wl,-plugin-opt,cache-policy="
 			policy := "cache_size=10%:cache_size_bytes=10g"
-			flags.LdFlags = append(flags.LdFlags, cachePolicyFormat+policy)
+			flags.Local.LdFlags = append(flags.Local.LdFlags, cachePolicyFormat+policy)
 		}
 
 		// If the module does not have a profile, be conservative and do not inline
